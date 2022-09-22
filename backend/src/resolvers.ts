2cc7272ebae4dc14de07a9b258ea2428da8d0bd0
Ryan Zheng <nogrethumphrey@gmail.com>
2022-09-22 06:54:47 +0200

    remove babel from frontend

--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -6,14 +6,49 @@ let service = new JobSearchService();
 
 export const resolvers = {
     Query: {
+        /**
+         * Jobs Query Resolvers
+         */
         searchJobs: () => service.searchJobsByPageNo(0),
         jobDetail(jobId: String) {
             console.log("query job detail jobId:" + jobId)
             return service.queryJobDetail(jobId);
         }
+
+        /**
+         * User Query Resolvers
+         */
+
+
+        /**
+         * Membership Query Resolvers
+         */
+
+
+        /**
+         * Community Story Query Resolvers
+         */
+
     },
 
     Mutation: {
+        /**
+         * Jobs Query Resolvers
+        */
+
+        /**
+         * User Query Resolvers
+         */
+
+
+        /**
+         * Membership Query Resolvers
+         */
+
+
+        /**
+         * Community Story Query Resolvers
+         */
 
     }
 };
