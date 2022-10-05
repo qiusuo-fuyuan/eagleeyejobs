cf77be4aa66cb018c38ada245749d3d4180a4576
liyanzhang505 <liyzyouxiang@126.com>
2022-10-05 23:01:02 +0800

    issue-9/job-model-api: fix variables

--- backend/src/repositories/JobRepository.ts
+++ backend/src/repositories/JobRepository.ts
@@ -25,13 +25,13 @@ export class JobRepository extends BaseRepository{
         if (size == undefined || size <= 0) {
             size = 20
         }
-        var count = await this.JobDocumentModel.count({
+        let count = await this.JobDocumentModel.count({
             title: {$regex: new RegExp(titleKeyword, 'i')},
             isDeleted: {$ne: 1}
         })
-        var jobs = await this.JobDocumentModel.find({title: {$regex: new RegExp(titleKeyword, 'i')}, isDeleted: {$ne: 1}})
+        let jobs = await this.JobDocumentModel.find({title: {$regex: new RegExp(titleKeyword, 'i')}, isDeleted: {$ne: 1}})
             .skip(size * (page - 1)).limit(size).sort({'updatedAt': -1, '_id': -1})
-        var jobsOutput = new JobPageOutput()
+        let jobsOutput = new JobPageOutput()
         jobsOutput.size = size
         jobsOutput.current = page
         jobsOutput.total = count % size == 0 ? count / size : Math.floor(count / size) + 1
