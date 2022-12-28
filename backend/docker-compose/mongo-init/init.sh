83810985ffa829290f7d5aa8d95a23dec9ab7063
Ryan Zheng <nogrethumphrey@gmail.com>
2022-12-28 08:23:27 +0100

    main: use only one node for mongodb

--- backend/docker-compose/mongo-init/init.sh
+++ backend/docker-compose/mongo-init/init.sh
@@ -5,19 +5,16 @@ until [ ! $RET -ne 0 ]; do
     mongo --host mongo1:27017 --eval \
         '
         db = (new Mongo("mongo1:27017")).getDB("test");
-        config = {
-            "_id" : "eagleeyejobs-replica-set",
-            "members" : [
-            {
-                "_id" : 0,
-                "host" : "mongo1:27017"
-            },
-            {
-            "_id" : 1,
-            "host" : "mongo2:27017"
-            }
-            ]
-        };
+            config = {
+                "_id" : "eagleeyejobs-replica-set",
+                "members" : [
+                {
+                    "_id" : 0,
+                    "host" : "mongo1:27017",
+                    "priority": 100,
+                }
+                ]
+            };
         rs.initiate(config);';
     RET=$?;
     echo $RET
